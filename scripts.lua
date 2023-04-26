bcbc9de54e11f048f84e2467160daadb39c443c7
prosko1234 <131814064+prosko1234@users.noreply.github.com>
2023-04-26 10:22:45 +0300

    Create scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,26 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("CB:RO hub(by prosko1234)", "DarkTheme")
+
+-- MAIN
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Main")
+
+MainSection:NewButton("Stormy", "good script", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ik3ac/stormy-fixed-counter-blox-2023/main/.gitignore"))()
+end)
+
+MainSection:NewButton("Anti kick", "nice script", function()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/prosko1234/antikick/main/antikick.lua"),true))()
+end)
+
+MainSection:NewButton("King hub", "ButtonInfo", function()
+    loadstring(game:HttpGet(('https://pastebin.com/raw/R8QMbhzv')))()
+end)
+
+MainSection:NewButton("Ominous Vibe", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/loadstring.lua"))()
+end)
+
+MainSection:NewButton("Morph", "Troll people", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ChronoAccelerator/Public-Scripts/main/Morphing/MorphScript.lua"))()
+end)
